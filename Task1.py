841c822c79fbb1cc3b7d4d7a9a40e45cc2fa8396
SeyitCenan <106025852+SeyitCenan@users.noreply.github.com>
2022-07-17 22:57:46 +0300

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect,flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 from flask_wtf import FlaskForm
@@ -24,44 +24,53 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
-def password_check(passwd):
+
+def password_check(pswa):
+
         SpecialSym =['$', '@', '#', '%','.',',','*']
-        if len(passwd) < 7:
+        
+        if(len(str(pswa))<8):
             return 'length should be at least 8'
             
-        if len(passwd) > 20:
+        elif(len(str(pswa))> 20):
             return 'length should be not be greater than 20'
             
-        if not any(char.isdigit() for char in passwd):
-            return 'Password should have at least one numeral'
+        elif not any(char.isdigit() for char in str(pswa)):
+            flash('Password should have at least one numeral')
+            return redirect(url_for('register'))
             
-        if not any(char.isupper() for char in passwd):
+         
+        elif not any(char.isupper() for char in str(pswa)):
             return 'Password should have at least one uppercase letter'
             
-        if not any(char.islower() for char in passwd):
+        elif not any(char.islower() for char in pswa):
             return 'Password should have at least one lowercase letter'
             
-        if not any(char in SpecialSym for char in passwd):
+        
+        elif not any(char in SpecialSym for char in pswa):
             return 'Password should have at least one of the symbols $@#'
-        return "Valid"
+        else:    
+            return "Valid"
 
 
 class RegisterForm(FlaskForm):
+
     username = StringField(validators=[ InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
 
     password = PasswordField(validators=[InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
 
     submit = SubmitField('Register')
-    def validate_username(self, username):
+    def validate_username(self,username):
         existing_user_username = User.query.filter_by( username=username.data).first()
         if existing_user_username:
             raise ValidationError('That username already exists. Please choose a different one.')
-    def validate_password(self,password):
-        result = password_check(password.data)
+    def validate_password(self,psw):
+        b=str(psw)
+        result = password_check(b)
         if (result == "Valid"):
          return redirect(url_for('login'))
         else:
-            return redirect(url_for('register'))
+         return redirect(url_for('register'))
     
 class LoginForm(FlaskForm):
     username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
@@ -70,11 +79,9 @@ class LoginForm(FlaskForm):
     
     submit = SubmitField('Login')
 
-
 @app.route('/')
 def home():
     return render_template('home.html')
-
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -87,13 +94,10 @@ def login():
                 return redirect(url_for('dashboard'))  
     return render_template('login.html', form=form)
         
-
-
 @app.route('/dashboard', methods=['GET', 'POST'])
 @login_required
 def dashboard():
     return render_template('dashboard.html')
-
 
 @app.route('/logout', methods=['GET', 'POST'])
 @login_required
@@ -101,12 +105,14 @@ def logout():
     logout_user()
     return redirect(url_for('login'))
 
-
 @ app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
-
+    
+    psw=form.password.data
+    a=str(psw)
     if form.validate_on_submit():
+        form.validate_password(a)
         hashed_password = bcrypt.generate_password_hash(form.password.data)
         new_user = User(username=form.username.data, password=hashed_password)
         db.session.add(new_user)
@@ -115,6 +121,6 @@ def register():
 
     return render_template('register.html', form=form)
 
-
 if __name__ == "__main__":
     app.run(debug=True) 
+    
